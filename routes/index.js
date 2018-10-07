557db6b903acd0eabed8bfcf9f2912fc231626e9
bobsta16 <bobsta16@hotmail.com>
2018-10-07 21:47:38 +1300

    Good stuff

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,137 @@ var express = require("express");
 var router = express.Router();
 var url = "mongodb://localhost:27017/";
 var mongo = require("mongodb");
+const MongoClient = mongo.MongoClient;
+const dbName = 'GoOut';
+var passport = require('passport');
 
-router.get('/', function(req, res){
-    res.render('index');
+function isAuthenticated(req,res,next){
+   if(req.user)
+      return next();
+   else
+        res.redirect('/login');
+}
+
+function getEvents(user, callback) {
+    MongoClient.connect(url, function(err, client) {
+        var events=[];
+        const db = client.db(dbName);
+        var collection = db.collection('Attendance');
+        collection.find({user_id: new mongo.ObjectID(user._id)}).toArray(function(err,result) {
+            if(err) throw err;
+            collection = db.collection('Event');
+            var counter = 0;
+            for(var i=0;i<result.length;i++) {
+                events.push({going:result[i].going,eventInfo: {}});
+                collection.findOne({_id: new mongo.ObjectID(result[i].event_id)}, function(err, eventResult) {
+                    events[counter].eventInfo = eventResult;
+                    counter++;
+                    if(counter == result.length) {
+                        client.close();
+                        callback(events);
+                    }
+                });
+            }
+        });
+    });
+} 
+
+function getEvent(eventID, callback) {
+    MongoClient.connect(url, function(err, client) {
+        console.log("Finding event")
+        const db = client.db(dbName);
+        var collection = db.collection('Event');
+        collection.findOne({_id: new mongo.ObjectID(eventID)}, function(err, result) {
+            if(err) throw err;
+            callback(result);
+        });
+    });
+}
+
+router.get('/', isAuthenticated, function(req, res){
+    getEvents(req.user, function(events) {
+        res.render('index', {event: events});
+    })
 });
+
+router.get('/event/:eventID', isAuthenticated, function(req,res) {
+    getEvent(req.params.eventID,function(event) {
+        console.log("rendering");
+        res.render('event', event);
+    })
+});
+
+router.get('/login', function(req,res) {
+    res.render('loginPage');
+});
+
+router.get('/addAccount', function(req,res) {
+    res.render('addAccount');
+});
+
+router.get('/addEvent', isAuthenticated, function(req,res) {
+    res.render('addEvent');
+});
+
+router.post('/addEvent',isAuthenticated, function(req,res) {
+    MongoClient.connect(url, function(err, client) {
+        const db = client.db(dbName);
+        var collection = db.collection('Event');
+        const event = {
+            eventName: req.body.eventName,
+            address: req.body.address,
+            images: [],
+            date: req.body.date,
+            requirements: req.body.requirements,
+            cost: req.body.cost,
+            maxPeople: req.body.maxPeople,
+            private: req.body.private,
+            time: req.body.time
+        };
+        collection.insertOne(event, function(err) {
+            if(err) throw err;
+            console.log("1 event inserted");
+            collection = db.collection('Attendance');
+            const attendance = {
+                user_id: req.user._id,
+                event_id: event._id,
+                host: 'true',
+                going: 'yes'
+            }
+            collection.insertOne(attendance, function(err) {
+                if(err) throw err;
+                console.log("1 attendance inserted");
+                client.close();
+            });
+        });
+        
+        res.redirect('/');
+    });
+});
+
+router.post('/addAccount', function(req,res) {
+    MongoClient.connect(url, function(err, client) {
+        const db = client.db(dbName);
+        const collection = db.collection('User');
+        const user = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            age: req.body.age,
+            email: req.body.email,
+            password: req.body.password,
+            phone: ""
+        };
+        collection.insertOne(user, function(err) {
+            if(err) throw err;
+            console.log("1 user inserted");
+            client.close();
+        });
+        res.redirect('/login');
+    });
+});
+    
+router.post('/login'
+            ,passport.authenticate('local', { successRedirect: '/',
+                                              failureRedirect: '/login'}));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,13 +2,21 @@ var express = require("express");
 var app     = express();
 var routes = require('./routes/index');
 var hbs = require('express-handlebars');
+var bodyParser = require('body-parser');
 var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var MongoClient = mongo.MongoClient;
+var url = 'mongodb://localhost:27017/GoOut';
+var dbName = 'GoOut';
+var session = require('express-session');
+
 /**var passport = require('passport');
 var util = require('util');
 var FacebookStrategy = require('passport-facebook').Strategy;
 var config = require('./configuration/config');
 
-var session = require('express-session');
+
 var fs = require('fs');
 var key = fs.readFileSync('encryption/domain-key.txt');
 var cert = fs.readFileSync('encryption/domain-crt.txt');
@@ -19,14 +27,6 @@ cert: cert,
 ca: ca
 };
 var https = require('https');
-*/
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-/**
 passport.use(new FacebookStrategy({
     clientID: config.facebook_api_key,
     clientSecret:config.facebook_api_secret ,
@@ -49,14 +49,61 @@ app.use(session({secret: 'secretCookie',
                  resave: false,
                  saveUninitialized: false}));
 */
+
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passReqToCallback : true
+},
+    function(req, email, password, done) {
+        console.log("Starts authentication");
+        MongoClient.connect(url, function(err, client) {
+            const db = client.db(dbName);
+            const collection = db.collection('User');
+            console.log("Connects to database");
+            collection.findOne({'email':email, 'password':password},function(err,user) {
+                if(err) { return done(err); }
+                if(!user) {
+                    console.log("Fail")
+                    return done(null, false, {message: 'Incorrect Email or Password.'});
+                }
+                console.log("Success")
+                return done(null,user);
+            });
+        })
+    }
+));
+
 app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(session({ 
+    secret: 'ladhirrim'/**,
+    resave: true,
+    saveUninitialized: true,*/
+}));
+app.use(bodyParser.urlencoded({extended: false }));
+app.use(bodyParser.json());
+
+passport.serializeUser(function(user, done) {
+    console.log("Serialising user");
+  done(null, user._id);
+});
+passport.deserializeUser(function(id, done) {
+   MongoClient.connect(url, function(err, client) {
+        const db = client.db(dbName);
+        const collection = db.collection('User');
+        const result = collection.findOne({'_id':new mongo.ObjectID(id)},function(err,user) {
+            console.log("deserialising user");
+            return done(err, user);
+        });
+   });
+});
 
 app.use(express.static(__dirname + '/public'));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', routes);
 
